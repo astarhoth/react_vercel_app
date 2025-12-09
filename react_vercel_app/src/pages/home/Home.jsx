cdde3fb195e0ea95e8c5a7f0d346bc83a412bae4
astarhoth <mariavitoriabrayner@gmail.com>
2025-12-09 09:42:30 -0300

    projeto final

--- react_vercel_app/src/pages/home/Home.jsx
+++ react_vercel_app/src/pages/home/Home.jsx
@@ -3,62 +3,45 @@ import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 export default function Home() {
-  const [books, setBooks] = useState([]);
-  const [filtered, setFiltered] = useState([]);
-  const [search, setSearch] = useState("");
-
+  const [book, setBook] = useState(null);
   const navigate = useNavigate();
 
-  // Carrega todos os livros
-  async function fetchAllBooks() {
-    const res = await fetch("https://potterapi-fedeperin.vercel.app/en/books");
-    const data = await res.json();
-    setBooks(data);
-    setFiltered(data);
-  }
-
-  // Filtro da pesquisa
-  function handleSearch(text) {
-    setSearch(text);
-    setFiltered(
-      books.filter((book) =>
-        book.originalTitle.toLowerCase().includes(text.toLowerCase())
-      )
-    );
-  }
-
-  useEffect(() => {
-    async function loadBooks() {
-      await fetchAllBooks();
+  
+  async function fetchRandomBook() {
+    try {
+      const res = await fetch("https://potterapi-fedeperin.vercel.app/en/books/random");
+      const data = await res.json();
+      setBook(data);
+    } catch (err) {
+      console.error("Erro ao buscar livro:", err);
     }
-    loadBooks();
-  }, []);
+  }
+
+  
+ useEffect(() => {
+  const load = async () => {
+    await fetchRandomBook();
+  };
+  load();
+}, []);
+
+
+  if (!book) return <p>Carregando...</p>;
 
   return (
     <div className="home-container">
-      <h1 className="home-title">Livros de Harry Potter</h1>
+      <h1>Livro {book.number} - {book.originalTitle}</h1>
 
-      <input
-        type="text"
-        className="search-input"
-        placeholder="Pesquisar por título..."
-        value={search}
-        onChange={(e) => handleSearch(e.target.value)}
+      <img
+        src={book.cover}
+        alt={book.originalTitle}
+        className="book-cover"
+        onClick={() => navigate("/detalhes", { state: { book } })}
       />
 
-      <div className="books-grid">
-        {filtered.map((book) => (
-          <div
-            key={book.number}
-            className="book-card"
-            onClick={() => navigate("/detalhes", { state: { book } })}
-          >
-            <img src={book.cover} alt={book.originalTitle} />
-            <h3>Livro {book.number}</h3>
-            <p>{book.originalTitle}</p>
-          </div>
-        ))}
-      </div>
+      
+      <button className="reload-btn" onClick={fetchRandomBook}>
+        Novo Livro
+      </button>
     </div>
-  );
-}
+  );}
